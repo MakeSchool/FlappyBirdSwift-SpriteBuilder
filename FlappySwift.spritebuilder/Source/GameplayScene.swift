019dc225a1f10bc19c486a3823648f455b7832cb
Daniel Haaser <thedapperdan@gmail.com>
2015-01-06 09:13:30 -0800

    emptied GameplayScene

--- FlappySwift.spritebuilder/Source/GameplayScene.swift
+++ FlappySwift.spritebuilder/Source/GameplayScene.swift
@@ -5,7 +5,6 @@ class GameplayScene: TutorialScene
     
     // put variables below this line
     
-    var timeSinceObstacle:CCTime = 0
     
     // put variables above this line
     
@@ -13,45 +12,12 @@ class GameplayScene: TutorialScene
     {
         // put your initialization code below this line
         
-        character = Character.createFlappy()  
-        physicsNode.addChild(character)
-        
-        self.showScore()
         
         // put your initialization code above this line
     }
     
     // put new methods below this line
     
-    override func tap()
-    {
-        var impulse:CGFloat = 200 - 2 * character.physicsBody.velocity.y
-        character.physicsBody.applyImpulse(CGPoint(x:0, y:impulse))
-    }
-    
-    override func update(delta: CCTime)
-    {
-        character.physicsBody.velocity = CGPoint(x:80.0, y:character.physicsBody.velocity.y)
-        
-        timeSinceObstacle += delta;
-        
-        if timeSinceObstacle > 2
-        {
-            self.addObstacle()
-            timeSinceObstacle = 0
-        }
-    }
-    
-    func ccPhysicsCollisionBegin(pair: CCPhysicsCollisionPair, character nodeA: CCNode, level nodeB: CCNode) -> ObjCBool
-    {
-        self.gameOver()
-        return true
-    }
-    
-    func ccPhysicsCollisionBegin(pair: CCPhysicsCollisionPair, character nodeA: CCNode, goal nodeB: CCNode) -> ObjCBool {
-        self.increaseScore()
-        return false
-    }
     
     // put new methods above this line
 }
